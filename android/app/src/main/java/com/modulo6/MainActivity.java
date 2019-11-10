bf72656eaad7c1cc914ba12a63a83302d4c2eb28
NaluFigueira <anachfigueira@gmail.com>
2019-11-09 21:45:42 -0300

    Iniciando o desafio

---
+++ android/app/src/main/java/com/modulo6/MainActivity.java
@@ -0,0 +1,25 @@
+package com.modulo6;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "modulo6";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+}
